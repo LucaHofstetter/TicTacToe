8d867280c283f15a8b5785042b5b566f122b2466
LucaHofstetter <75506452+LucaHofstetter@users.noreply.github.com>
2022-05-23 10:45:21 +0200

    Gameboard can now safe x/o values in Array

--- src/Board.tsx
+++ src/Board.tsx
@@ -2,8 +2,24 @@ import React from "react";
 import Square from "./Square";
 
 export default class Board extends React.Component {
-  renderSquare(i: number) {
-    return <Square value={i} />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
   render() {
     const status = "Next player: X";
